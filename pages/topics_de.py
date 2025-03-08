03cda9fdee76d9f9120eea6465759a1e62301199
Narges Chinichian <19248728+NargesChinichian@users.noreply.github.com>
2025-03-08 22:17:14 +0100

    Initiate DE topics.

--- pages/topics_de.py
+++ pages/topics_de.py
@@ -1,4 +1,15 @@
 import streamlit as st
 from pages.sidebar import load_sidebar  # Import the sidebar function
+
+
+
+## Show in webpage
+st.header("Topics Network")
+
 load_sidebar()
+# Display the Dash app using an iframe
+st.components.v1.iframe("", height=700, scrolling=True)
+
+
+
 
