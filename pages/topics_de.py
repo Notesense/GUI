4771ea187346d9f9bce36ed0386cba48194bb1b0
Narges Chinichian <19248728+NargesChinichian@users.noreply.github.com>
2025-03-10 11:33:44 +0100

    Change the order on German page.

--- pages/topics_de.py
+++ pages/topics_de.py
@@ -4,11 +4,12 @@ from pages.sidebar import load_sidebar  # Import the sidebar function
 
 
 ## Show in webpage
-st.header("Topics Network")
+#st.header("Topics Network")
+
+# Display the Dash app using an iframe
+st.components.v1.iframe("", height=850, scrolling=True)
 
 load_sidebar()
-# Display the Dash app using an iframe
-st.components.v1.iframe("", height=700, scrolling=True)
 
 
 
