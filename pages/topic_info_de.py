94a339d3ba5b55e67a5983fec21565808c6b2088
Narges Chinichian <19248728+NargesChinichian@users.noreply.github.com>
2025-03-18 21:38:14 +0100

    Change download to each topic.

--- pages/topic_info_de.py
+++ pages/topic_info_de.py
@@ -5,27 +5,45 @@ from pages.sidebar import load_sidebar  # Import the sidebar function
 
 st.title("Topics Visualization")
 # Load and display the saved HTML file
-st.markdown("The following 16 topics are found using the [BERTopic](https://maartengr.github.io/BERTopic/index.html) model with min_topic_size set to the rounded 1 percent of the total number of valid notes after preprocessing (45934 total notes --> 459 minimum size of topic) in German language. You can download the note ids together with their topics as a .csv file if you click on the download button below.")
+st.markdown("The following 16 topics are found using the [BERTopic](https://maartengr.github.io/BERTopic/index.html) model with min_topic_size set to the rounded 1 percent of the total number of valid notes after preprocessing (45934 total notes --> 459 minimum size of topic) in German language. You can download the note ids associated to each topic in the dropdown below. The data will be downloaded as a .csv file.")
 load_sidebar()
+
 csv_data = pd.read_csv("data/noteId_Topic_Keywords_de.csv")
+# Filter out Topic -1
+csv_data = csv_data[csv_data['Topic'] >= 0]
 
-# # Display the CSV table
-# st.write("### CSV Data Preview")
-# st.dataframe(csv_data)
+# Add a dropdown for topic selection
+unique_topics = sorted(csv_data['Topic'].unique())
+selected_topic = st.selectbox("Select a Topic to Download", unique_topics)
 
-# Create a CSV download link
+# Filter data by selected topic
+filtered_data = csv_data[csv_data['Topic'] == selected_topic]
+
+# Create CSV download link for filtered data
 @st.cache_data
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
-csv_bytes = convert_df(csv_data)
+filtered_csv_bytes = convert_df(filtered_data)
 
 st.download_button(
-    label="Download Note IDs and Topics as a CSV",
-    data=csv_bytes,
-    file_name="topics_data.csv",
+    label=f"Download Topic {selected_topic} Data as CSV",
+    data=filtered_csv_bytes,
+    file_name=f"topic_{selected_topic}_data.csv",
     mime="text/csv",
 )
+
+
+
+# # Display the CSV table
+# st.write("### CSV Data Preview")
+# st.dataframe(csv_data)
+# st.download_button(
+#     label="Download Note IDs and Topics as a CSV",
+#     data=csv_bytes,
+#     file_name="topics_data.csv",
+#     mime="text/csv",
+# )
 
 # Load the HTML content
 with open("data/barchart_de.html", "r") as f:
@@ -41,7 +59,6 @@ html_wrapper = f"""
 """
 
 
-load_sidebar()
 st.components.v1.html(html_wrapper, height=900, scrolling=False)
 
 st.markdown("""
